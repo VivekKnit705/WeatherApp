2aa8ef842e80b736fe9c4638f813ea7594a841b8
VivekKnit705 <vivek.knit69@gmail.com>
2022-03-26 19:21:45 +0530

    Wallpaper update: show the backGround image according to time

--- app.js
+++ app.js
@@ -5,49 +5,78 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
+const body = document.querySelector('body');
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 const API_KEY = '6e53d4d6ea398500db3b226bfc9786fa';
 
+
+
 setInterval(() => {
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hoursIn12HrFormate = hour >= 13 ? hour % 12 : hour;
-    const minute = time.getMinutes();
-    const ampm = hour >= 12 ? 'PM' : 'AM';
-    console.log(ampm);
-    timeEl.innerHTML = (hoursIn12HrFormate < 10 ? '0' + hoursIn12HrFormate : hoursIn12HrFormate) + ' : ' + (minute < 10 ? '0' + minute : minute) + `<span id="am-pm">${ampm}</span>`
-    dateEl.innerHTML = days[day] + ',' + date + ' ' + months[month];
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HrFormate = hour >= 13 ? hour % 12 : hour;
+  const minute = time.getMinutes();
+  const ampm = hour >= 12 ? 'PM' : 'AM';
+  console.log(ampm);
+  timeEl.innerHTML = (hoursIn12HrFormate < 10 ? '0' + hoursIn12HrFormate : hoursIn12HrFormate) + ' : ' + (minute < 10 ? '0' + minute : minute) + `<span id="am-pm">${ampm}</span>`
+  dateEl.innerHTML = days[day] + ',' + date + ' ' + months[month];
+  if (hour > 19 && hour < 4) {
+    body.style.background = "url('https://source.unsplash.com/random/900×700/?night')";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+
+  }
+  else if (hour > 4 && hour < 10) {
+    body.style.background = "url('https://source.unsplash.com/random/900×700/?sunset')";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+
+  }
+  else if (hour > 10 && hour < 16) {
+    body.style.background = "url('https://source.unsplash.com/random/900×700/?afternoon')";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+
+
+  }
+  else if (hour > 16 && hour < 19) {
+    body.style.background = "url('https://source.unsplash.com/random/900×700/?sunset')";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+
+  }
+
 }, 1000)
 
 getWeatherData()
 
 function getWeatherData() {
-    navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
-        let { latitude, longitude } = success.coords;
-        console.log(latitude, longitude);
+  navigator.geolocation.getCurrentPosition((success) => {
+    console.log(success);
+    let { latitude, longitude } = success.coords;
+    console.log(latitude, longitude);
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=houarly,minutely&units=metric&appid=${API_KEY}`)
-            .then(res => res.json()).then(data => {
-                console.log(data);
-                showWeatherData(data);
-            })
-    })
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=houarly,minutely&units=metric&appid=${API_KEY}`)
+      .then(res => res.json()).then(data => {
+        console.log(data);
+        showWeatherData(data);
+      })
+  })
 }
 
 function showWeatherData(data) {
-    let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
-    timezone.innerHTML = data.timezone;
-    countryEl.innerHTML = data.lat + 'N  ' + data.lon + 'E';
+  let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
+  timezone.innerHTML = data.timezone;
+  countryEl.innerHTML = data.lat + 'N  ' + data.lon + 'E';
 
 
-    currentWeatherItemEl.innerHTML =
-        `<div class="weather-item">
+  currentWeatherItemEl.innerHTML =
+    `<div class="weather-item">
     <div>Humidity</div>
     <div>${humidity}</div>
   </div>
@@ -67,13 +96,13 @@ function showWeatherData(data) {
   <div>Sunset</div>
   <div>${window.moment(sunset * 1000).format('HH: mm a')}</div>
 </div>`
-        ;
+    ;
 
-    let othersDayForecast = '';
-    data.daily.forEach((day, idx) => {
-        if (idx == 0) {
-            currentTempEl.innerHTML =
-                `<img
+  let othersDayForecast = '';
+  data.daily.forEach((day, idx) => {
+    if (idx == 0) {
+      currentTempEl.innerHTML =
+        `<img
             src=" http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
             alt="weather icon"
             class="w-icon"
@@ -84,9 +113,9 @@ function showWeatherData(data) {
             <div class="temp">Day-${day.temp.day}&#176; c</div>
           </div>`
 
-        } else {
-            othersDayForecast +=
-                `<div class="weather-forecast-item">
+    } else {
+      othersDayForecast +=
+        `<div class="weather-forecast-item">
             <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
             <img
               src=" http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png "
@@ -96,10 +125,11 @@ function showWeatherData(data) {
             <div class="temp">Night - ${day.temp.night}&#176; c</div>
             <div class="temp">Day- - ${day.temp.day}&#176; c</div>
           </div>`
-        }
-    });
+    }
+  });
 
-    weatherForecastEl.innerHTML = othersDayForecast;
+
+  weatherForecastEl.innerHTML = othersDayForecast;
 }
 
 console.log(window.innerHeight);
